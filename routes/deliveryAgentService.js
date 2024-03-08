cc81e80b5481c2f871e9eed79d92cab1ab56e665
ayushsingh07 <nqu7069@gmail.com>
2024-03-08 22:56:49 +0530

    delivery_agent

--- routes/deliveryAgentService.js
+++ routes/deliveryAgentService.js
@@ -47,4 +47,26 @@ router.put('/:orderId', async (req, res) => {
     }
 });
 
+
+// router.put('/deliveryagent/:id/available', async (req, res) => {
+//     const deliveryAgentId = req.params.id;
+
+//     try {
+//         // Find the delivery agent by ID
+//         const deliveryAgent = await DeliveryAgent.findById(deliveryAgentId);
+//         if (!deliveryAgent) {
+//             return res.status(404).json({ error: `Delivery agent with ID ${deliveryAgentId} not found` });
+//         }
+
+//         // Update the status of the delivery agent to available
+//         deliveryAgent.status = 'available';
+//         await deliveryAgent.save();
+
+//         res.status(200).json({ message: `Delivery agent with ID ${deliveryAgentId} is now available` });
+//     } catch (error) {
+//         console.error("Error marking delivery agent as available:", error);
+//         res.status(500).json({ error: "Internal server error" });
+//     }
+// });
+
 module.exports = router;
