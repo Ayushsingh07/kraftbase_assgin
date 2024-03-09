76273238c9b0d92a936a214e80c75b7556c665c1
ayushsingh07 <nqu7069@gmail.com>
2024-03-09 12:33:05 +0530

    port changed

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => { // Listen on any available IP address
     console.log(`Server is running on port ${PORT}`);
 });
